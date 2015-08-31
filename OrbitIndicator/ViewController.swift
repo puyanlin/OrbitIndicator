e5087fca93a9971ef3ba13aaab6082f591312c97
Joseph Lin <puyan.tw@gmail.com>
2015-08-31 23:25:08 +0800

    add files

---
+++ OrbitIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrbitIndicator
+//
+//  Created by Puyan Lin on 2015/8/31.
+//  Copyright (c) 2015年 Puyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
