f86d09fd1a057ec89a13199e622f62341daafde9
Joseph Lin <puyan.tw@gmail.com>
2015-08-31 23:42:03 +0800

    add orbit

--- OrbitIndicator/ViewController.swift
+++ OrbitIndicator/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgindicator: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(10, animations: { () -> Void in
+            self.imgindicator.layer.transform = CATransform3DMakeRotation( CGFloat( 90.0/180 * M_PI ) , 0, 0, 1);
+        })
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
